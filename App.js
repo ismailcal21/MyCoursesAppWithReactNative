9891a4b4ebd7d0ba6ba1270ed75c6ba245ffd245
ismailcal21 <ismailcal21@gmail.com>
2023-08-06 14:11:56 -0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import CourseInput from "./components/CourseInput";
 
 export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [text, setText] = useState([]);
+
+  const startModal = () => {
+    setModalIsVisible(true);
+  };
+
+  const endModal = () => {
+    setModalIsVisible(false);
+  };
+
+  console.log(modalIsVisible);
+
+  const addCourse = (courseTitle) => {
+    console.log(courseTitle);
+    setText((currentCourses) => [
+      ...currentCourses,
+      { text: courseTitle, id: Math.random().toString() },
+    ]);
+    endModal();
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+
+      <View style={styles.container}>
+        <Button title="Kurs Ekle" color="red" onPress={startModal} />
+
+        <CourseInput
+          visible={modalIsVisible}
+          onAddCourse={addCourse}
+          onCancel={endModal}
+        />
+        <View>
+          <FlatList
+            data={text}
+            renderItem={({ item }) => (
+              <View style={styles.courseItem}>
+                <Text style={styles.courseText}>{item.text}</Text>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  courseText: {
+    padding: 8,
+    color: "white",
+  },
+  courseItem: {
+    backgroundColor: "gray",
+    margin: 8,
+    borderRadius: 10,
   },
 });
